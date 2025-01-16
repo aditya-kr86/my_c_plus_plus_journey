d1224d8fb75543f37aa258f7b36e3fc24307d9a0
Aditya Kumar <162503610+aditya-kr86@users.noreply.github.com>
2025-01-16 23:53:38 +0530

    Update tut48.cpp

--- tut48.cpp
+++ tut48.cpp
@@ -1,7 +1,76 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    
+/*
+case 1:
+class B : public A{
+ A-->B //Order of Execution
+}
+case 2:
+class A : public B, public C{
+ B-->C-->A //Order of Execution
+}
+case 2:
+class A : public B,virtual public C{
+ C-->B-->A //Order of Execution
+}
+
+*/
+
+class Base1
+{
+    int data1;
+
+public:
+    Base1(int i)
+    {
+        data1 = i;
+        cout << "Base1 class constructor called" << endl;
+    }
+
+    void printDataBase1(void)
+    {
+        cout << "the value of data1 is " << data1 << endl;
+    }
+};
+
+class Base2
+{
+    int data2;
+
+public:
+    Base2(int i)
+    {
+        data2 = i;
+        cout << "Base2 class constructor called" << endl;
+    }
+
+    void printDataBase2(void)
+    {
+        cout << "the value of data2 is " << data2 << endl;
+    }
+};
+
+class Derived: public Base1,public Base2{
+    int derived1, derived2;
+    public:
+    Derived(int a, int b, int c, int d) : Base1(a), Base2(b){
+        derived1 = c;
+        derived2 = d;
+        cout<<"The Dervied Class constructor is called" <<endl;
+    }
+    void printDataDerived(void)
+    {
+        cout<<"The value of derived1 is "<< derived1 <<endl;
+        cout<<"The value of derived2 is "<< derived2 <<endl;
+    }
+};
+
+int main()
+{
+    Derived d(1,2,3,4);
+    d.printDataDerived();
+    d.printDataBase1();
+    d.printDataBase2();
     return 0;
 }
