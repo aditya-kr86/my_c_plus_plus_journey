2913de56cb97b884eed0a38a28f30d21edfa76dd
Aditya Kumar <162503610+aditya-kr86@users.noreply.github.com>
2025-01-17 00:19:35 +0530

    Re-Update tut45.cpp

--- tut45.cpp
+++ tut45.cpp
@@ -1,77 +1,61 @@
 #include <iostream>
 using namespace std;
 
-class Student
-{
+class Student {
 protected:
     int roll_no;
-
 public:
-    void set_number(int a)
-    {
+    void set_number(int a) {
         roll_no = a;
     }
-    void print_number(void)
-    {
+    void print_number(void) {
         cout << "Your roll no is " << roll_no << endl;
     }
 };
 
-class Test : public Student
-{
+class Test : virtual public Student { // Use virtual inheritance here
 protected:
     float maths, physics;
-
 public:
-    void set_marks(float m1, float m2)
-    {
+    void set_marks(float m1, float m2) {
         maths = m1;
         physics = m2;
     }
-
-    void print_marks(void)
-    {
-        cout << "You result is here: " << endl
+    void print_marks(void) {
+        cout << "Your result is here: " << endl
              << "Maths: " << maths << endl
              << "Physics: " << physics << endl;
     }
 };
 
-class Sports : public Student
-{
+class Sports : virtual public Student { // Use virtual inheritance here
 protected:
     float score;
-
 public:
-    void set_score(float sc)
-    {
+    void set_score(float sc) {
         score = sc;
     }
-
-    void print_score(void)
-    {
+    void print_score(void) {
         cout << "Your PT score is " << score << endl;
     }
 };
 
-class Result : public Test, public Sports
-{
+class Result : public Test, public Sports {
 private:
     float total;
-
 public:
-    void display(void)
-    {
+    void display(void) {
         total = maths + physics + score;
-        print_number();
+        print_number();  // Now unambiguous due to virtual inheritance
         print_marks();
         print_score();
         cout << "Your total score is: " << total << endl;
     }
 };
-int main(){
+
+int main() {
     Result harry;
-    harry.set_number(4200);
+    harry.set_number(4200);        // Unambiguous
     harry.set_marks(78.9, 99.5);
     harry.set_score(9);
     harry.display();
