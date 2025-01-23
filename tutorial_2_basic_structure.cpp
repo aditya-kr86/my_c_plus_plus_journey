b4353fd4f26590b8066bd8f636041c6b9b86e9ea
Aditya Kumar <162503610+aditya-kr86@users.noreply.github.com>
2025-01-23 21:41:51 +0530

    Intermediate Commit

---
+++ tutorial_2_basic_structure.cpp
@@ -0,0 +1,27 @@
+// Key Components of the Code:
+#include <iostream>
+int main() {
+    std::cout << "Hello World";
+    return 0;
+}
+/*
+1. Header File (#include <iostream>):
+   - Adds libraries for input/output.
+   - Example: std::cout for output.
+
+2. Blank Lines:
+   - Ignored by the compiler.
+   - Improve code readability.
+
+3. int main():
+   - Entry point of the program.
+   - Must return an integer (e.g., return 0;).
+
+4. std::cout << "Text";:
+   - Displays output text.
+   - Use std::cout with << for printing.
+   - Text enclosed in double quotes is a string literal.
+
+5. return 0;:
+   - Signals successful program execution.
+  */
