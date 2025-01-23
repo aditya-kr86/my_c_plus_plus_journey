8c01f282046d6b725a6d1b040e6c4c77f04c021b
Aditya Kumar <162503610+aditya-kr86@users.noreply.github.com>
2025-01-23 21:33:56 +0530

    Intermediate Commit

---
+++ tutorial_1_introduction.c
@@ -0,0 +1,17 @@
+/*What is Programming and Why C++?
+-->Programming: Giving instructions to a computer to solve problems.
+
+Why C++?:
+-->Efficient and close to hardware.
+
+-->Object-Oriented, ideal for real-world systems.
+-->Still widely used despite being developed in the 1980s.
+*/
+
+// First Program:
+
+#include <iostream>
+int main() {
+    std::cout << "Hello World";
+    return 0;
+}
