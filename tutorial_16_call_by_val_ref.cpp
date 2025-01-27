30f484b5622806b3ffc81e3838c0a7e94f6f9b19
Aditya Kumar <adityakumargupta082003@gmail.com>
2025-01-27 23:18:09 +0530

    Intermediate Commit

---
+++ tutorial_16_call_by_val_ref.cpp
@@ -0,0 +1,59 @@
+/*
+
+
+# Comparison Table
+--------------------
+
+
+Method              | Parameter Passed      | Affects Original Values?  | Use Case
+-----------------------------------------------------------------------------------------------------------------------------------------------
+                    |                       |                           |
+Call by Value       | Copy of variables     | No                        | When original values must not change.
+                    |                       |                           |   
+Call by Pointer     | Address of variables  | Yes                       | When you need to modify the original values using pointers.
+                    |                       |                           |   
+Call by Reference   | Reference variables   | Yes                       | When you need to modify the original values without using pointers.
+
+*/
+#include<iostream>
+using namespace std;
+
+int sum(int a, int b){
+    int c = a + b;
+    return c;
+}
+
+// This will not swap a and b
+void swap(int a, int b){ //temp a b
+    int temp = a;        //4   4  5   
+    a = b;               //4   5  5
+    b = temp;            //4   5  4 
+}
+
+// Call by reference using pointers
+void swapPointer(int* a, int* b){ //temp a b
+    int temp = *a;          //4   4  5   
+    *a = *b;               //4   5  5
+    *b = temp;            //4   5  4 
+}
+
+// Call by reference using C++ reference Variables
+// int & 
+void swapReferenceVar(int &a, int &b){ //temp a b
+    int temp = a;          //4   4  5   
+    a = b;               //4   5  5
+    b = temp;            //4   5  4 
+    // return a;
+}
+
+int main(){
+    int x =4, y=5;
+    // cout<<"The sum of 4 and 5 is "<<sum(a, b);
+    cout<<"The value of x is "<<x<<" and the value of y is "<<y<<endl;
+    // swap(x, y); // This will not swap a and b
+    // swapPointer(&x, &y); //This will swap a and b using pointer reference
+    swapReferenceVar(x, y); //This will swap a and b using reference variables
+    // swapReferenceVar(x, y) = 766; //This will swap a and b using reference variables
+    cout<<"The value of x is "<<x<<" and the value of y is "<<y<<endl; 
+    return 0;
+}
