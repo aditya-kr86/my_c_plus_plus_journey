40d8531e47eca45597676dde33a7404d2c520b2e
Aditya Kumar <adityakumargupta082003@gmail.com>
2025-01-27 23:19:00 +0530

    Intermediate Commit

---
+++ tutorial_20_oops_intro.cpp
@@ -0,0 +1,19 @@
+/*
+1. Why OOP?:
+- Procedural programming lacks data security and maintainability.
+- OOP models real-world problems using classes and objects.
+
+2. Core Concepts:
+- Class: Template for objects.
+- Object: Instance of a class.
+- Encapsulation: Wrapping data & methods together.
+- Inheritance: Reusing properties of one class in another.
+- Polymorphism: One interface, multiple functionalities.
+- Dynamic Binding: Code decided at runtime.
+- Message Passing: Object communication.
+
+3. Benefits:
+- Secure data handling.
+- Better code reuse and modularity.
+- Simplifies complex software management.
+*/
