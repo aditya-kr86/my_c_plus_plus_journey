cd4902de06feae88a07e6eb52dfda4ccadd86ff1
Aditya Kumar <adityakumargupta082003@gmail.com>
2025-02-04 23:39:11 +0530

    Intermediate Commit

--- tutorial_52_obj_array_using_ptr.cpp
+++ tutorial_52_obj_array_using_ptr.cpp
@@ -1,3 +1,15 @@
+/*
+1. Array of Objects:
+- Stores objects as elements.
+- Use pointers to dynamically allocate arrays.
+2. Code Snippet:
+3. Input Loop:
+- Use ptr->setData() and increment ptr .
+4. Output Loop:
+- Use a temporary pointer ptrTemp for getData() .
+5. Key Concept:
+- Increment pointers to ensure different objects are accessed.
+*/
 #include <iostream>
 using namespace std;
 
