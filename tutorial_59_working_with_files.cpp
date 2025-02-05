185cb1053248623312d373c0319f28ed6e24253d
Aditya Kumar <adityakumargupta082003@gmail.com>
2025-02-05 23:53:37 +0530

    Intermediate Commit

---
+++ tutorial_59_working_with_files.cpp
@@ -0,0 +1,10 @@
+/*
+# Key File Operations in C++
+- Reading a File → Extracts data from a file.
+- Writing to a File → Saves data into a file.
+
+# Important C++ File I/O Functions
+- cin >> → Takes input from the user.
+- cout << → Displays output on the screen.
+- fstream → Handles file operations in C++.
+*/
