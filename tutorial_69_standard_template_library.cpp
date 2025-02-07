ce575a6f857bc585b5fa2217564b3e6bfa7b776b
Aditya Kumar <adityakumargupta082003@gmail.com>
2025-02-07 15:24:11 +0530

    Intermediate Commit

---
+++ tutorial_69_standard_template_library.cpp
@@ -0,0 +1,19 @@
+/*
+# What is STL?
+- STL (Standard Template Library) is a collection of generic classes and functions for efficient programming.
+- It is widely used in competitive programming to save time by providing pre-implemented data structures and algorithms.
+
+# Why Use STL?
+- Avoids reinventing the wheel – Use pre-built components instead of writing everything from scratch.
+- Improves efficiency in coding contests and interviews.
+- Reduces development time while ensuring reliability.- 
+
+# Components of STL
+- Containers – Store data (e.g., vector, list, map).
+- Algorithms – Manipulate data (e.g., sort(), search(), reverse()).
+- Iterators – Access and traverse data (similar to pointers).- 
+
+# How STL Works?
+- Iterators connect algorithms to containers for efficient data manipulation.
+- Example: Sorting a container using sort() and iterators.
+*/
