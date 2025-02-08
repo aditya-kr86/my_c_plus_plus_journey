ce48771e93ee3a47c6e131ffb1eb097fa0a0bd67
Aditya Kumar <adityakumargupta082003@gmail.com>
2025-02-08 23:28:18 +0530

    Intermediate Commit

--- tutorial_74_function_objects_in_cpp_stl.cpp
+++ tutorial_74_function_objects_in_cpp_stl.cpp
@@ -1,3 +1,11 @@
+/*
+- Functors are objects that behave like functions.
+- Used in STL algorithms to customize behavior.
+- Default sorting is ascending, but we can pass a functor to change it.
+- Example: greater<int>() sorts an array in descending order.
+- Other functors include plus<T>(), minus<T>(), multiplies<T>(), etc.
+- Explore more STL functors from: C++ Reference - Function Objects.
+*/
 #include<iostream>
 #include<functional>
 #include<algorithm>
